04734c702ada3ccd32bd34996d1b511a1d92525f
harsh.kakashaniya <harsh.kakashniya@gmail.com>
2018-11-18 15:00:31 -0500

    Doc:Matlab Code added

---
+++ Report/Matlab Code/spline2dparts.m
@@ -0,0 +1,107 @@
+clear all
+clear
+n=60; % Points to be considered in curve
+X=linspace(1,15,n); % equal spacing of points
+noise=(10)*(rand()-0.5);  % noise in data from -1 to 1
+Y=(0.1).*X.^3 - (1.5)*X.^2 + (6).*X + noise; % noisy output
+
+Camerax=[2 4 6 8 12 14 ]
+Cameray=[0 10 -5 20 20 80]
+Scale1=0.5
+Scale2=0.5
+Scale3=0.1
+%##################### Without section ##########################
+
+Xn=linspace(1,15,n);
+S4=Xn.^3*transpose(Xn); % X^4 terms sum
+S3=Xn.^2*transpose(Xn); % X^3 terms sum
+S2=Xn*transpose(Xn); % X^2 terms sum
+S1=Xn*ones(n,1); % X terms sum
+%   AX=B 
+An=[S4 S3 S2;   
+   S3 S2 S1;
+   S2 S1 1];
+
+Bn=[Y*transpose(Xn.^2);Y*transpose(Xn);Y*ones(n,1)];
+Constant1 = inv(An)*Bn; % by least square method
+
+Ylinen=(Constant1(1,1).*Xn.^2 + Constant1(2,1).*Xn + Constant1(3,1)) ;
+errorn=Y-Ylinen; % Error term in actual and computed
+
+%################Section wise ##################
+
+%for first section
+X1=linspace(1,5,n/3);
+
+S4=X1.^3*transpose(X1); % X^4 terms sum
+S3=X1.^2*transpose(X1); % X^3 terms sum
+S2=X1*transpose(X1); % X^2 terms sum
+S1=X1*ones(n/3,1); % X terms sum
+
+%   AX=B 
+A1=[S4 S3 S2;   
+   S3 S2 S1;
+   S2 S1 1];
+
+B1=[Y(1:n/3)*transpose(X1.^2);Y(1:n/3)*transpose(X1);Y(1:n/3)*ones(n/3,1)];
+ConstCamera1=inv([Camerax(1,1) 1;Camerax(1,2) 1])*[Camerax(1,1);Camerax(1,2)];
+Constant1=inv(A1)*B1; % by least square method
+
+Yline1=(Constant1(1,1).*X1.^2 + Constant1(2,1).*X1 + Constant1(3,1))+ Scale1*(ConstCamera1(1,1)*X1+ConstCamera1(2,1));
+error1=Y(1:n/3)-Yline1; % Error term in actual and computed
+
+%################################################################
+%for second section
+X2=linspace(5,10,n/3);
+
+S4=X2.^3*transpose(X2); % X^4 terms sum
+S3=X2.^2*transpose(X2); % X^3 terms sum
+S2=X2*transpose(X2); % X^2 terms sum
+S1=X2*ones(n/3,1); % X terms sum
+
+%   AX=B 
+A2=[S4 S3 S2;   
+   S3 S2 S1;
+   S2 S1 1];
+
+B2=[Y(((n/3)+1):2*n/3)*transpose(X2.^2);Y(((n/3)+1):2*n/3)*transpose(X2);Y(((n/3)+1):2*n/3)*ones(n/3,1)];
+ConstCamera2=inv([Camerax(1,3) 1;Camerax(1,4) 1])*[Camerax(1,3);Camerax(1,4)];
+Constant2=inv(A2)*B2; % by least square method
+
+Yline2=(Constant2(1,1).*X2.^2 + Constant2(2,1).*X2 + Constant2(3,1))+Scale2*(ConstCamera2(1,1)*X1+ConstCamera2(2,1));
+error2=Y(((n/3)+1):2*n/3)-Yline2; % Error term in actual and computed
+
+%################################################################
+
+%for third section
+X3=linspace(10,15,n/3);
+
+S4=X3.^3*transpose(X3); % X^4 terms sum
+S3=X3.^2*transpose(X3); % X^3 terms sum
+S2=X3*transpose(X3); % X^2 terms sum
+S1=X3*ones(n/3,1); % X terms sum
+
+%   AX=B 
+A3=[S4 S3 S2;   
+   S3 S2 S1;
+   S2 S1 1];
+
+B3=[Y(((2*n/3)+1):n)*transpose(X3.^2);Y(((2*n/3)+1):n)*transpose(X3);Y(((2*n/3)+1):n)*ones(n/3,1)];
+ConstCamera3=inv([Camerax(1,5) 1;Camerax(1,6) 1])*[Camerax(1,5);Camerax(1,6)];
+Constant3=inv(A3)*B3; % by least square method
+
+Yline3=(Constant3(1,1).*X3.^2 + Constant3(2,1).*X3 + Constant3(3,1))+Scale3*(ConstCamera3(1,1)*X1+ConstCamera3(2,1));
+error3=Y(((2*n/3)+1):n)-Yline3; % Error term in actual and computed
+
+%######################## Graph section ###################
+
+error=[error1 error2 error3 ];
+Yline=[Yline1 Yline2 Yline3];
+Xf=[X1 X2 X3];
+figure;
+hold on
+plot(Xf,Yline,'-or')
+plot(X,Y,'+-b',Xn,Ylinen,'-og');grid;
+plot(Camerax,Cameray,'*k')
+figure;
+plot(X,abs(error),'-g',X,abs(errorn),'-r');grid;
---
+++ Report/Matlab Code/spline3d.m
@@ -0,0 +1,69 @@
+%##################### Initialize ##########################
+clear all
+clear
+n=20 % Points to be considered in curve
+t=linspace(0,5,n) % equal spacing of points
+
+%######################## Noise ############################
+
+noisex=5*(rand(1,n)-0.5) %noise in x
+noisey=5*(rand(1,n)-0.5) %noise in y
+noisez=5*(rand(1,n)-0.5) %noise in z
+
+%####################### Noisy Data ########################
+
+%X=3.*t.^2 + 5.*t + 2 + noisex %noisy data in x 
+%Y=2.*t.^2 + 7.*t + 2 + noisey %noisy data in y
+%Z=3.*t.^2 + 4.*t + 2 + noisez %noisy data in z
+%######################## Data set #########################
+X=[20 18 16 14 12 10 8 6 4 2 1 2 4 6 8 10 12 14 16 20]
+Y=[1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20]
+Z=[1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20]
+%################### Summation matrix ######################
+
+S4=t.^3*transpose(t) % t^4 terms sum
+S3=t.^2*transpose(t) % t^3 terms sum
+S2=t*transpose(t) % t^2 terms sum
+S1=t*ones(n,1) % X terms sum
+
+%################ Least Square matrix ######################
+%   AX=B 
+A=[S4 S3 S2;  %A matrix for computing X matrix  
+   S3 S2 S1;
+   S2 S1 1]
+
+Bx=[X*transpose(t.^2);X*transpose(t);Y*ones(n,1)] %B in x
+By=[Y*transpose(t.^2);Y*transpose(t);Y*ones(n,1)] %B in y
+Bz=[Z*transpose(t.^2);Z*transpose(t);Z*ones(n,1)] %B in z
+
+Constantx=inv(A)*Bx % by least square method x
+Constanty=inv(A)*By % by least square method y
+Constantz=inv(A)*Bz % by least square method z
+
+%########## Computing value of new co-ordinates ###########
+
+Ylinex=Constantx(1,1).*t.^2 + Constantx(2,1).*t + Constantx(3,1) %X point
+Yliney=Constanty(1,1).*t.^2 + Constanty(2,1).*t + Constanty(3,1) %Y point
+Ylinez=Constantz(1,1).*t.^2 + Constantz(2,1).*t + Constantz(3,1) %Z point
+
+errorx= X-Ylinex % error in x
+errory= Y-Yliney % error in y
+errorz= Z-Ylinez % error in z
+
+error = abs(((errorx).^2 + errory.^2 + errorz.^2).^(0.5)) % final error
+%##################### Plots #############################
+figure;
+plot3(X,Y,Z,'o-b');grid; %plot of noisy data
+hold on 
+plot3(Ylinex,Yliney,Ylinez,'--r');grid; %plot of least square data
+figure;
+plot(t,error,'-g');grid; % Error wrt to time
+figure;
+plot(X,t,'--b')
+%####################### Velocity ########################
+
+Vx=6.*t + 5 %velcity data in x 
+Vy=4.*t + 7 %velcity data in y
+Vz=6.*t + 4 %velcity data in z
+figure;
+plot(t,Vx,'-g',t,Vy,'-r',t,Vz,'-b');grid;
